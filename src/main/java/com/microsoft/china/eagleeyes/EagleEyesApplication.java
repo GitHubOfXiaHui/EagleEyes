92083099c441d866fdbeed51e9b4f66dcf4c49f2
Hui Xia <t-huxi@microsoft.com>
2018-01-23 16:40:24 +0800

    init

---
+++ src/main/java/com/microsoft/china/eagleeyes/EagleEyesApplication.java
@@ -0,0 +1,12 @@
+package com.microsoft.china.eagleeyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleEyesApplication.class, args);
+	}
+}
