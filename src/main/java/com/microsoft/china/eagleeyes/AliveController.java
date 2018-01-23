38dd4d965191f8924e062a3d3a2a12bf11b618e0
Hui Xia <t-huxi@microsoft.com>
2018-01-23 16:55:40 +0800

    alive

---
+++ src/main/java/com/microsoft/china/eagleeyes/AliveController.java
@@ -0,0 +1,15 @@
+package com.microsoft.china.eagleeyes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AliveController {
+	
+	@RequestMapping(path = "alive", method = RequestMethod.GET)
+	public String alive() {
+		return "alive";
+	}
+
+}
